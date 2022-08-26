5b49dda3325400cb50d7e16825195ecaa75c4909
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-08-26 20:08:31 +0530

    #

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-array_2D = [["John", "A"], ["James", "B"], ["Thomas", "C"]]
+array_2D = [["Name: John", "Grade: A"], ["Name: James", "Grade: B"], ["Name: Thomas", "Grade: C"]]
 array_3D = [[["John", "A", "Excellent"], ["James", "B", "Very Good"], ["Thomas", "C", "Good"]]]
 
-print(array_2D[0])
+print(array_2D[0][1])
