909f3bb2d85e2f946d50e3b3de0733b402c25aea
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-10-07 20:01:00 +0530

    changes

--- add_with_five.py
+++ add_with_five.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter import messagebox
 
 root = Tk()
 root.geometry("400x400")
@@ -8,13 +7,12 @@ root.title("Add any number with five")
 label1 = Label(root)
 input_box = Entry(root)
 
-def add():
+def add()
 	try:
-		input_box_data = int(input_box.get())
 		result1 = input_box_data + 5
 		label1["text"] = result1
 	except ValueError:
-		messagebox.showerror("Error!", "Please put a valid number")
+		label1["text"] = "Please put a valid number"
 
 btn = Button(root, text="Click me to add!", command=add)
 input_box.pack()
