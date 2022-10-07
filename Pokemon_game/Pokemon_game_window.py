33127acc634561cef0633ba02534a49cdb1da0c0
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-10-07 10:25:40 +0530

    Update Pokemon game
    Just loaded the images

--- Pokemon_game/Pokemon_game_window.py
+++ Pokemon_game/Pokemon_game_window.py
@@ -1,6 +1,8 @@
+#Librarys
 from tkinter import *
 from PIL import Image, ImageTk
 
+#Make the window
 root = Tk()
 root.title("Pokemon game")
 root.geometry("600x400")
@@ -10,10 +12,28 @@ root.configure(background="sky blue")
 abra = ImageTk.PhotoImage(Image.open ("abra.jpg"))
 bulbasour = ImageTk.PhotoImage(Image.open ("bulbasour.jpg"))
 button = ImageTk.PhotoImage(Image.open ("button.jpg"))
-pikachu = ImageTk.PhotoImage(Image.open ("picachu.jpg"))
-#abra = ImageTk.PhotoImage(Image.open ("abra.jpg"))
-#abra = ImageTk.PhotoImage(Image.open ("abra.jpg"))
+pikachu = ImageTk.PhotoImage(Image.open ("pikachu.jpg"))
+jigglypuff = ImageTk.PhotoImage(Image.open("jigglypuff.jpg"))
+Iyvasour = ImageTk.PhotoImage(Image.open("Iyvasour.jpg"))
+charmender = ImageTk.PhotoImage(Image.open("charmender.jpg"))
+kadabra = ImageTk.PhotoImage(Image.open("kadabra.jpg"))
+meowth = ImageTk.PhotoImage(Image.open("meowth.jpg"))
+persion = ImageTk.PhotoImage(Image.open ("persion.jpg"))
+ratata = ImageTk.PhotoImage(Image.open("ratata.jpg"))
+squirtle = ImageTk.PhotoImage(Image.open("squirtle.jpg"))
+nidoking = ImageTk.PhotoImage(Image.open("nidoking.jpg"))
+paras = ImageTk.PhotoImage(Image.open ("paras.jpg"))
 
+#Powers and cards
+pokemon_list = ["Pikachu", "Bulbasor", "jigglypuff", "Iyvasour", "charmender", "kadabra", "meowth", "persion", "ratata", "squirtle", "nidoking", "paras"]
+Power_pokemon = [200, 60, 70, 100, 50, 70, 60, 70, 40, 50, 90, 40]
+
+#Other
+player1_score_data = 0
+def player1_s_function():
+	global player1_score_data
+	random_number_player1 = random.randint(0, 13)
+	pokemon_list[random_number_player1]
 
 #Labels
 player1 = Label(root, text="Player 1")
