1a5f55b2b3ff3c2ca155304a0191fbe2a49ff63f
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-10-25 19:50:36 +0530

    DELETE!

---
+++ Question_paper_for_daisy.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry("500x550")
+root.title("Question for Daisy's percentage to vist the doctor")
+
+
+
+root.mainloop()
