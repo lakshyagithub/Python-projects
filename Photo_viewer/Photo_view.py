be04d9364c31d9bac2d96f54ed40f2cec60b442d
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-11-01 19:31:55 +0530

    Commit Changes

--- Photo_viewer/Photo_view.py
+++ Photo_viewer/Photo_view.py
@@ -11,21 +11,23 @@ root.title("Photo viewer by Lakshya")
 
 label_image = Label(root, bg="yellow", highlightthickness=5)
 label_image.place(relx=0.5, rely=0.5, anchor=CENTER)
-
+img_path=""
 def open_image():
-    img_path = filedialog.askopenfile(title="open image", filetypes=[(
+    global img_path
+    img_path = filedialog.askopenfilename(title="open image", filetypes=[(
         "Image files",
         "*.png; *.jpg; *.jpeg; *.gif; *.txt"
         )])
     print("Image path: ", img_path)
-    img = ImageTk.PhotoImage(Image.open(img_path))
+    img1=Image.open(img_path)
+    img = ImageTk.PhotoImage(img1)
     label_image["image"]=img
     img.close()
   
 open_file_btn = Button(root, text="Open file", command=open_image, bg="light green", fg="white", bd=0, font=("Comic Sans MS", 12, "bold"))
 open_file_btn.place(relx=0.5, rely=0.7, anchor=CENTER)
 
-rotate_file_btn = Button(root, text="Rotate image", command=open_image, bg="light green", fg="white", bd=0, font=("Comic Sans MS", 12, "bold"))
+rotate_file_btn = Button(root, text="Rotate image", bg="light green", fg="white", bd=0, font=("Comic Sans MS", 12, "bold"))
 rotate_file_btn.place(relx=0.5, rely=0.8, anchor=CENTER)
 
 root.mainloop()
--- Planet/planet.py
+++ Planet/planet.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from tkinter import ttk
 
 root = Tk()
 root.title("About planets")
@@ -7,17 +9,22 @@ icon1 = PhotoImage(file="Logo.png")
 root.iconphoto(False, icon1)
 root.configure(background="sky blue")
 
-label_planet = Label(root, bg="sky blue", font="Comic San MS", text="Planet: ")
-label_planet_name = Label(root, bg="sky blue", font="Comic San MS", text="")
-label_planet_gravity = Label(root, bg="sky blue", font="Comic San MS", text="gravity: ")
-label_planet_info = Label(root, bg="sky blue", font="Comic San MS")
+label_planet = Label(root, bg="sky blue", font=("Comic Sans MS", 12, "bold"), text="Planet: ")
+label_planet_name = Label(root, bg="sky blue", font=("Comic Sans MS", 12, "bold"), text="")
+label_planet_gravity = Label(root, bg="sky blue", font=("Comic Sans MS", 12, "bold"), text="gravity: ")
+label_planet_info = Label(root, bg="sky blue", font=("Comic Sans MS", 12, "bold"))
+
 
 def showinfoplanets():
     print("Complete this function.")
-    
-label_planet.pack()
-label_planet_name.pack()
-label_planet_gravity.pack()
-label_planet_info.pack()
+
+
+btn = Button(root, text="Show planet info", command=showinfoplanets, bd=0, bg="light green", font=("Comic Sans MS", 12, "bold"))
+
+label_planet.place(relx=0.2, rely=0.1, anchor=CENTER)
+btn.place(relx=0.5, rely=0.18, anchor=CENTER)
+label_planet_name.place(relx=0.5, rely=0.25, anchor=CENTER)
+label_planet_gravity.place(relx=0.5, rely=0.8, anchor=CENTER)
+label_planet_info.place(relx=0.5, rely=0.9, anchor=CENTER)
 
 root.mainloop()
