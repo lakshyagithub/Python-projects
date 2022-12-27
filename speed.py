f7e6d6faab5ac9ea62137c2c66944d0d782d8ad8
lakshyagithub <lakshyasc2708@gmail.com>
2022-12-27 20:02:53 +0530

    Commit

--- speed.py
+++ speed.py
@@ -30,4 +30,14 @@ label_download_speed.place(relx=0.25, rely=0.6, anchor=CENTER)
 label_upload_speed = Label(root, font=("Yu Gothic Light", 14, "bold"), bg="#dee8f1")
 label_upload_speed.place(relx=0.25, rely=0.6, anchor=CENTER)
 
+def speedtest():
+    st = speedtest.Speedtest()
+    download_speed = round(st.download()/1000000, 2)
+    label_download_speed["text"] = str(download_speed) + "mbps"
+    upload_speed = round(st.upload()/1000000, 2)
+    label_upload_speed["text"] = str(upload_speed) + "mbps"
+    
+btn_doctor = Button(root, text="Check speed", bg="#218796", fg="white", relief=FLAT)
+btn_doctor.place(relx=0.5, rely=0.3, anchor=CENTER)
+
 root.mainloop()
