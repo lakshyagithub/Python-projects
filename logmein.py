8bf3f9c85f39f634aff365da6e3d1215b42fa1e1
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-12-28 16:28:45 +0000

    commit (incompleat)

---
+++ logmein.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x400")
+root.title("log me in")
+
+justforvscode = "VS code"
+label_user = Label(root, text="User name:")
+label_password = Label(root, )
+
+root.mainloop()
