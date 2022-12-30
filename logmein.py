1daef067e2d8804fca8721901f77159728ebc5c2
lakshyagithub <lakshyasc2708@gmail.com>
2022-12-30 09:41:43 +0530

    almost_done

--- logmein.py
+++ logmein.py
@@ -22,10 +22,22 @@ label_captcha.place(relx=0.2, rely=0.3, anchor=CENTER)
 captcha_textbox = Entry(root)
 captcha_textbox.place(relx=0.5, rely=0.3, anchor=CENTER)
 
+#Private elements
+name_private = Label(root, text="Name: ")
+name_private.place(relx=0.2, rely=0.6, anchor=CENTER)
+password_private = Label(root, text="Password: ")
+password_private.place(relx=0.2, rely=0.7, anchor=CENTER)
+captcha_private = Label(root, text="Captcha: ")
+captcha_private.place(relx=0.2, rely=0.8, anchor=CENTER)
+
 #Main code
 
 #Buttons - Update_login_details
-username_textbox = Button()
-username_textbox.place(relx=0.5, rely=0.1, anchor=CENTER)
+update_login_details = Button(root, text="Update login details")
+update_login_details.place(relx=0.2, rely=0.5, anchor=CENTER)
+
+#Buttons - Show_profile
+show_profile = Button(root, text="Show profile")
+show_profile.place(relx=0.8, rely=0.5, anchor=CENTER)
 
 root.mainloop()
