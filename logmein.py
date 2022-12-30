a60b6caf146904cdb1dba24d78d043ccc50ca7f0
lakshyagithub <lakshyasc2708@gmail.com>
2022-12-30 09:06:36 +0530

    Message

--- logmein.py
+++ logmein.py
@@ -4,12 +4,28 @@ root = Tk()
 root.geometry("400x400")
 root.title("log me in")
 
-#User name
+#Username
 label_user = Label(root, text="Username:")
-label_user.place(relx=0.3, rely=0.1, anchor=CENTER)
+label_user.place(relx=0.2, rely=0.1, anchor=CENTER)
 username_textbox = Entry(root)
 username_textbox.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-label_password = Label(root, text="Password: ")
+#Password
+label_password = Label(root, text="Password:")
+label_password.place(relx=0.2, rely=0.2, anchor=CENTER)
+password_textbox = Entry(root)
+password_textbox.place(relx=0.5, rely=0.2, anchor=CENTER)
+
+#Captcha
+label_captcha = Label(root, text="Captcha:")
+label_captcha.place(relx=0.2, rely=0.3, anchor=CENTER)
+captcha_textbox = Entry(root)
+captcha_textbox.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+#Main code
+
+#Buttons - Update_login_details
+username_textbox = Button()
+username_textbox.place(relx=0.5, rely=0.1, anchor=CENTER)
 
 root.mainloop()
