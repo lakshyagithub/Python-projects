32a31bbce5f94621192c9e4fa485f9372c4938d6
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2023-08-11 20:52:23 +0530

    230

---
+++ project_230/main.py
@@ -0,0 +1,8 @@
+# Task 1
+import pandas as pd
+
+df = pd.read_csv("country_vaccinations.csv")
+
+print("Shape of the given dataset:", df.shape) # Task 2
+print("Count of column:", df.columns) # Task 3
+print("Name of parameter used:", df.columns) # Task 4
