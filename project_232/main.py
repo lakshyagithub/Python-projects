2c21f3e10f47cc615a6a6bd68645e692a544c8bc
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2023-08-20 15:14:41 +0530

    sdsdsdsdsd

--- project_232/main.py
+++ project_232/main.py
@@ -1,16 +1,21 @@
 from numpy import loadtxt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
 
 dataset = loadtxt("pokemon.csv", delimiter=",")
-x = dataset.iloc[:, 1:7]
-y = dataset.iloc[:, 8]
+
+x = dataset[:,0:8]
+y = dataset[:,8]
+
+print("value of X are:", x)
+print("value of Y are:", y)
 
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation="relu"))
 model.add(Dense(8, activation="relu"))
 model.add(Dense(1, activation="sigmoid"))
 model.compile(loss="binary_crossentropy", metrics=["accuracy"])
-model.fit(x, y, epochs=500, batch_size=100)
-for i in range(785,800):
+model.fit(x, y, epochs=250, batch_size=100)
+
+for i in range(5):
 	print(f'{x[i].tolist()} => {predictions[i]} expected: {y[i]}')
